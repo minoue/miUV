46de544a29799ef857623653fde34e223e352bf0
Michi <mtkinoue@gmail.com>
2018-05-01 12:10:05 +0100

    Add flipUV function

--- miUvUtils.py
+++ miUvUtils.py
@@ -6,7 +6,19 @@ from maya import mel
 def flipUVs(args):
     cmds.polyEditUV(scale=True, scaleU=-1, pivotU=0.5)
 
+    
+def flipUVsByLocal(*args):
+    sel = cmds.ls(sl=True, fl=True, long=True)
+    u = 0.0
+    for i in sel:
+        uv = cmds.polyEditUV(i, relative=False, q=True, uValue=True)
+        u += uv[0]
+    uAve = u / len(sel)
+    u_tile = int(str(uAve)[0])
+    flipAxis = u_tile + 0.5
+    cmds.polyEditUV(relative=False, pivotU=flipAxis, scaleU=-1)    
 
+    
 def scaleUVs():
     pass
 
